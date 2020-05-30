c2047dc22f921a272ae740f81cf5c0f5a89dd0b3
Aleksey Ozimkov <31370328+aozimkov@users.noreply.github.com>
2020-05-31 01:41:04 +0300

    initial commit
    Create first version of maketizer.py

---
+++ maketizer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+
+# Read current folder, get sorted list with *.png filnames
+def get_images_list():
+    path = os.path.dirname(os.path.abspath(__file__))
+    return os.listdir(path)
+
+# Make an easy html file with image
+def get_file_body(image, maket_length, current_index):
+    index = current_index + 1 if current_index < maket_length else ''
+    return """<html>
+<head>
+<title>Test</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+</head>
+<body style="padding: 0; margin: 0;">
+<a href="index{}.html"><img src="{}" width="100%"/></a>
+</body></html>
+""".format(index, image)
+
+# construct html-file filename
+def get_filename(index):
+    index_postfix = '' if index == 0 else index + 1 
+    return "index{}.html".format(index_postfix)
+
+#create an html file and put generated html content
+def create_html_file(index, body):
+    f = open(get_filename(index), "w")
+    f.write(body)
+    f.close()
+
+images_list = get_images_list()
+
+for index in range(len(images_list)):
+    image = images_list[index]
+    html  = get_file_body(image,index,len(images_list))
+    create_html_file(index, html)
